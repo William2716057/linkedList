29178b5eb0102e471d74149dca5cf3237613fe38
William <willspri@gmail.com>
2024-02-25 14:58:22 +1300

    added

---
+++ linkedList.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Main {
+        public static void main(String[] args) {
+                LinkedList<String> items = new LinkedList<String>();
+                items.add("box");
+                items.add("shoe");
+                items.add("computer");
+                items.add("horse");
+                items.add("car");
+                System.out.println(items);
+        }
+}
+
